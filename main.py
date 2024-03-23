bc10889d0b12a6a90ef7dbbe6843896e130e6780
profillinz <157835465+profillinz@users.noreply.github.com>
2024-03-23 09:41:54 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+# variable = a reusable container for storing a value
+#            a variable behaves as if it were the value it contains
+
+age = 30
+
+ 
+print("I'm going to be " + str(age) + " soon") 
+print("I'm going to be", age, "soon") 
+print(f"I'm going to be {age} soon")
+
+# INTEGER
+
+Age = 30
+players = 5
+quantity = 3
+
+print(f"You are {age} years old now")
+print(f"There are {players} in the field")
+print(f"She had to buy {quantity} oranges instead")
+
+
+# FLOAT
+
+gpa = 3.3
+distance = 6.5
+price = 14.99
+
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}km")
+print(f"He bought it for ${price}")
+
+# STRING
+
+name = "Teegee"
+food = "Rice"
+email = "Teegee123@gmail.com"
+
+print(f'Hello {name}')
+print(f"You like {food}")
+print(f"My email is: {email}")
+
+# BOOLEAN
+
+online = True
+for_sale = False
+running = True
+
+print(f"Are you online?: {online}")
+print(f"Is the item for sale?: {for_sale}")
+print(f"Were you running?: {running}")
+
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
